c3e08592a1cd6bd5d37242accc0e1e03a28e1329
Tudor Achim <t.achim@gmail.com>
2015-10-16 16:45:46 -0700

    dai: add marginals

--- pgm/dai.py
+++ pgm/dai.py
@@ -1,6 +1,21 @@
+import os
 import subprocess
 
+def exact_marginals(fg_filename):
+    assert fg_filename.endswith('.fg')
+    print 'Running libDAI'
+    path = os.path.join(os.path.dirname(__file__), 'run_jtree')
+    p = subprocess.Popen([path, fg_filename],
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE)
+    p.wait()
+    output = p.stdout.read().split('\n')
+    # line format ({xi}, (%f, %f))
+    marginals = [float(l.split()[2][:-2]) for l in output if l.startswith('(')]
+    return marginals
+
 def exact_log_partition(fg_filename):
+    assert False, "TODO tudor implement this"
     assert fg_filename.endswith('.fg')
     print 'Running libDAI'
     p = subprocess.Popen(['./run_libdai', fg_filename],
