596f6e02f112af7926a557c2a587a0647ad0a856
Tudor Achim <t.achim@gmail.com>
2015-11-02 18:22:36 -0800

    trwbp update

--- pgm/dai.py
+++ pgm/dai.py
@@ -30,12 +30,13 @@ def exact_log_partition(fg_filename):
 def trwbp_upper_bound(fg_filename):
     assert fg_filename.endswith('.fg')
     print 'Running libDAI'
-    p = subprocess.Popen(['./run_trwbp', fg_filename],
+    path = os.path.join(os.path.dirname(__file__), 'run_trwbp')
+    p = subprocess.Popen([path, fg_filename],
             stderr=subprocess.PIPE,
             stdout=subprocess.PIPE)
     p.wait()
     print 'Done.'
     output = p.stdout.read().split('\n')
-    log_partition_line = [l for l in output if l.startswith('Log partition upper bound')]
+    log_partition_line = [l for l in output if l.startswith('Log partition')]
     upper_bound = float(log_partition_line[0].split()[-1])
     return upper_bound
