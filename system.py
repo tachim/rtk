51ff4bc0af81431b3522fec8782bba7993519bec
Tudor Achim <t.achim@gmail.com>
2015-12-02 09:41:28 -0800

    system: add option to print stdout as it comes in

--- system.py
+++ system.py
@@ -1,15 +1,18 @@
 import subprocess
 
-def run(cmd, capture_stdout=True):
+class RetCode(Exception): pass
+
+def run(cmd, capture_stdout=True, ignore_ret=False, print_stdout=False):
     if capture_stdout:
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         ret = []
         for line in iter(process.stdout.readline, ''):
             ret.append(line.strip())
-            print line.strip()
+            if print_stdout:
+                print line.strip()
         retval = process.wait()
-        assert retval == 0
+        if retval != 0 and not ignore_ret: raise RetCode()
         return '\n'.join(ret)
     else:
         retval = subprocess.Popen(cmd).wait()
-        assert retval == 0
+        if retval != 0 and not ignore_ret: raise RetCode()
