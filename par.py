794072c9f2cbea29cb0f6d6e59d11722c9dd22d8
Tudor Achim <t.achim@gmail.com>
2016-06-25 13:57:25 -0700

    par: output ETA

--- par.py
+++ par.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
 import numpy as np
 import random
+import time
 
 def wrapper_fcn((fn, (i, args_kwargs))):
     seed = int(random.random() * 100000)
@@ -8,7 +9,7 @@ def wrapper_fcn((fn, (i, args_kwargs))):
     random.seed(seed)
     return i, fn(*args_kwargs[0], **args_kwargs[1])
 
-def pmap(fn, args_kwargs, verbose=True, super_verbose=False):
+def pmap(fn, args_kwargs, verbose=True, super_verbose=False, n_procs=None):
     """ Multiprocessing abstraction that includes a progress meter.
         Usage:
             def foo(x): return x * 2
@@ -16,15 +17,24 @@ def pmap(fn, args_kwargs, verbose=True, super_verbose=False):
 
     """
     try:
-        p = mp.Pool(mp.cpu_count())
+        n_procs = n_procs or mp.cpu_count()
+        p = mp.Pool(n_procs)
         ret = [None] * len(args_kwargs)
 
         processed_args = [(fn, (i, a_k)) for i, a_k in enumerate(args_kwargs)]
 
+        start_time = time.time()
+
         for job_ind, (arg_ind, result) in \
                 enumerate(p.imap_unordered(wrapper_fcn, processed_args, chunksize=1)):
+
+            n_completed = job_ind + 1
+            avg_time = (time.time() - start_time) / n_completed
+            num_remaining = len(args_kwargs) - n_completed
+            eta = avg_time * num_remaining
+
             if verbose:
-                print '>>>>>>>>>>> PROGRESS:', job_ind+1, '/', len(args_kwargs), 'done'
+                print '>>>>>>>>>>> PROGRESS:', job_ind+1, '/', len(args_kwargs), 'done. ETA:', eta
             ret[arg_ind] = result
 
             if super_verbose:
@@ -39,7 +49,7 @@ def pmap(fn, args_kwargs, verbose=True, super_verbose=False):
         ret = []
         for i, (args, kwargs) in enumerate(args_kwargs):
             if verbose:
-                print '>>>>>>>>>>> PROGRESS:', i+1, '/', len(args_kwargs), 'done'
+                print '>>>>>>>>>>> SINGLE-THREAD PROGRESS:', i+1, '/', len(args_kwargs), 'done'
             ret.append(fn(*args, **kwargs))
 
         # Throw the original if we didn't hit any exception in the single-threaded execution
